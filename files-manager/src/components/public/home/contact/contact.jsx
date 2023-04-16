e8c826fd61003b2985b6cffed4e3a859d36a20e7
Abdulsalam Sawalha <109074523+AbdulsalamSaw@users.noreply.github.com>
2023-04-16 22:25:13 +0300

    add backend to contact us

--- files-manager/src/components/public/home/contact/contact.jsx
+++ files-manager/src/components/public/home/contact/contact.jsx
@@ -1,6 +1,51 @@
 import React from "react";
 import './contact.scss'
-function contact() {
+import axiosClient from "../../../../axios";
+import { useState } from "react";
+
+const Contact = () =>{
+    const [name, setName] = useState("");
+    const [emal, setEmail] = useState("");
+    const [subject, setSbject] = useState("");
+    const [message, setMessage] = useState("");
+
+  const handleFormSubmit = (event) => {
+    
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("emal", emal);
+    formData.append("subject", subject);
+    formData.append("message", message);
+    axiosClient.post("/owner/contact", formData)
+      .then((response) => {
+        alert("The Send Message successfully")
+        console.log(response.data);
+      })
+      .catch((error) => {
+        alert("fail")
+        console.log(error);
+      });
+  }
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    switch (id) {
+      case "name":
+        setName(value);
+        break;
+      case "email":
+        setEmail(value);
+        break;
+      case "subject":
+        setSbject(value);
+        break;
+        case "message":
+        setMessage(value);
+        break;
+      default:
+        break;
+    }
+  };
     return (
             <div class="container">
                 <div class="col-12 text-center">
@@ -9,39 +54,41 @@ function contact() {
                 <br/>
                 <div class="img_contact">
                     <div class="contact shadow p-3 mb-5 bg-white rounded">
-                        <form action="#" method="POST">
+                        <form onSubmit={handleFormSubmit}>
                             <div class="row">
                                 <h3 class="text-black mb-1">Contact Form</h3>
                             </div>
                             <div class="row">
                                 <div class="col-md-12 mb-3">
                                     <label for="name"> Name</label>
-                                    <input type="text" name="name" class="form-control rounded-0" placeholder="Abd" />
+                                    <input type="text"onChange={(e) => handleInputChange(e)} id="name" name="name" className="form-control rounded-0"  placeholder="Abd" />
+                                    <div className="error"></div>
                                 </div>
-                              
                             </div>
                             <div class="row">
                                 <div class="col-md-12 mb-3">
                                     <label for="email">Email</label>
-                                    <input type="text" name="email" class="form-control rounded-0" placeholder="@mail" />
+                                    <input type="text" onChange={(e) => handleInputChange(e)} id="email" name="email" className="form-control rounded-0"  placeholder="@mail" />
+                                    <div className="error"></div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12 mb-3">
                                     <label for="subject">Subject</label>
-                                    <input type="text" name="subject" class="form-control rounded-0" placeholder="Subject" />
+                                    <input type="text" onChange={(e) => handleInputChange(e)} id="subject" name="subject" className="form-control rounded-0"  placeholder="Subject" />
+                                    <div className="error"></div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12 mb-3">
                                     <label class="text-black" for="message">Message</label>
-                                    <textarea name="message" cols="30" rows="7" class="form-control rounded-0" placeholder="Leave your message here..."></textarea>
-
+                                    <textarea onChange={(e) => handleInputChange(e)} id="message" name="message" cols="30" rows="7" className="form-control rounded-0"  placeholder="Leave your message here..."></textarea>
+                                    <div className="error"></div>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="col-md-12 mb-3">
-                                    <button class="btn btn-primary rounded-pill" type="button" >Submit form</button>
+                                    <button class="btn btn-primary rounded-pill" type="submit" >Submit form</button>
                                 </div>
                             </div>
                         </form>
@@ -52,4 +99,4 @@ function contact() {
     );
 }
 
-export default contact;
+export default Contact;
